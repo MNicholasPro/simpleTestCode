431155130d8b9f31da7a4c26d004ac58368b3069
zhoumingkai <632549546@qq.com>
2017-01-23 18:28:21 +0800

    提交

---
+++ src/equals/duibi.java
@@ -0,0 +1,19 @@
+package equals;
+
+/**
+ * V1.0.0
+ * Created by zhoumingkai on 2017/1/23.
+ */
+public class duibi {
+    public static void main(String[] args){
+        Integer a = 1000;
+        Integer b = 1000;
+        Integer c = a;
+        int d = 1000;
+        System.out.println(a.equals(b));
+        System.out.println(a == c);
+        System.out.println(a == b);
+        System.out.println(a == d);
+
+    }
+}
